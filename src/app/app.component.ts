90ae96ec0dd7f8fb3c17a23a6d694b337bddea6d
vcarranza <viccarranza67@gmail.com>
2019-06-20 18:18:32 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ga-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'start-over';
+}
